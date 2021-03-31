a59434d656e7d5b37101c30a4ab785cdbbaa6118
Hexzel <a0972180845@gmail.com>
2021-03-30 22:37:02 -0700

    modify main.cpp and plot.py

--- HW2/main.cpp
+++ HW2/main.cpp
@@ -1,28 +1,32 @@
 // cut off frequency is 245.38 Hz
 
 #include "mbed.h"
+#include "uLCD_4DGL.h"
 using namespace std::chrono;
 
 DigitalOut led(LED1);
 AnalogOut aout(PA_4);
 AnalogIn ain(PC_5);
+InterruptIn up_btn(D9);
+InterruptIn down_btn(D10);
+InterruptIn ok_btn(D11);
+Timer debounce;
 Timeout f_ctrl;
-InterruptIn button(USER_BUTTON);
+uLCD_4DGL uLCD(D1, D0, D2);
 
 const uint16_t peak = 59577;	// f(v) = 65535 / 3.3 * v -> f(3) = 59577
 const uint16_t sample_number = 500;
+const uint16_t frq_num = 3;
 volatile bool lever;
-bool need_sample = 0;
+bool up = 0, down = 0, ok = 0;
 
-void turnDownLever(void)
-{
-	lever = 0;
-}
 
-void sendData(void)
-{
-	need_sample = 1;
-}
+void turnDownLever(void);
+void pushUp(void);
+void pushDown(void);
+void pushOk(void);
+void boundedChange(uint8_t *num, uint8_t bound, int8_t change);
+void moveArrow(uint8_t ith);
 
 int main()
 {
@@ -32,10 +36,26 @@ int main()
 	uint16_t delay_time[3] = {62, 183, 483};		// 62 -> 246 Hz
 	uint16_t v_in_index = sample_number;
 	bool need_print = 0;
-	uint8_t frq_index = 2;
+	uint8_t frq_index = 0;
+	uint8_t state = 0, pre_state = 0;
 
-	button.rise(&sendData);
+	up_btn.rise(&pushUp);
+    down_btn.rise(&pushDown);
+    ok_btn.rise(&pushOk);
+    debounce.start();
 	led = 0;
+	uLCD.printf("  Frquency table");
+	uLCD.locate(0, 2);
+	uLCD.printf("247.7 Hz");
+	uLCD.locate(0, 4);
+	uLCD.printf("100 Hz");
+	uLCD.locate(0, 6);
+	uLCD.printf("40 Hz");
+	uLCD.locate(10, 2);
+	uLCD.printf("<-");
+	uLCD.locate(0, 14);
+	uLCD.printf("    down  up  ok");
+
 	while (1) {
 		for (uint16_t i = 0; i < 50 ; i++) {
 			f_ctrl.attach_us(&turnDownLever, delay_time[frq_index]);
@@ -52,11 +72,34 @@ int main()
 			}
 			while (lever) ;
 		}
-		if (need_sample) {
-			v_in_index = 0;
-			need_sample = 0;
-			need_print = 1;
-			led = 1;
+		if (up || down || ok) {
+			if (ok && state == pre_state) {
+				v_in_index = 0;
+				need_print = 1;
+				led = 1;
+				uLCD.locate(0, 12);
+				uLCD.printf(" sending...      ");
+			}
+			while (!ok) {
+                if (up) {
+                    boundedChange(&state, frq_num, 1);
+                    up = 0;
+                } else if (down) {
+                    boundedChange(&state, frq_num, -1);
+                    down = 0;
+                }
+				ThisThread::sleep_for(50ms);
+				moveArrow(state);
+				uLCD.locate(0, 12);
+				uLCD.printf(" press ok to gen  ");
+            }
+            ok = 0;
+			pre_state = state;
+			frq_index = state;
+			if (!need_print) {
+				uLCD.locate(0, 12);
+				uLCD.printf(" press ok to send  ");
+			}
 		}
 		if (need_print &&  v_in_index >= sample_number) {
 			need_print = 0;
@@ -65,6 +108,54 @@ int main()
 				printf("%f\r\n", v_in[i]);
 			}
 			led = 0;
+			uLCD.locate(0, 12);
+			uLCD.printf(" sending complete  ");
 		}
 	}
+}
+
+void pushUp(void)
+{
+    if (duration_cast<milliseconds>(debounce.elapsed_time()).count() > 400) {
+        up = 1;
+        debounce.reset();
+    }
+}
+
+void pushDown(void)
+{
+    if (duration_cast<milliseconds>(debounce.elapsed_time()).count() > 400) {
+        down = 1;
+        debounce.reset();
+    }
+}
+
+void pushOk(void)
+{
+    if (duration_cast<milliseconds>(debounce.elapsed_time()).count() > 400) {
+        ok = 1;
+        debounce.reset();
+    }
+}
+
+void turnDownLever(void)
+{
+	lever = 0;
+}
+
+void boundedChange(uint8_t *num, uint8_t bound, int8_t change) {
+    change = change % bound;
+    if (*num + change < bound && *num + change >= 0) *num += change;
+    else if (change > 0) *num = change + *num - bound;
+    else if (change < 0) *num = *num + change + bound;
+}
+
+void moveArrow(uint8_t ith)
+{
+	for (uint8_t j = 1; j <= frq_num; j++) {
+		uLCD.locate(10, 2 * j);
+		uLCD.printf("  ");
+	}
+	uLCD.locate(10, 2 * (ith + 1));
+	uLCD.printf("<-");
 }
